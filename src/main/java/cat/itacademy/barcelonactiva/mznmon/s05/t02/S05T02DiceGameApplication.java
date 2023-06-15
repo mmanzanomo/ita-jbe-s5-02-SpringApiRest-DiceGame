e585b26bb905bad4e5382b1b845dba5c49e70b23
Miguel Ángel Manzano <mznmiguel@gmail.com>
2023-06-16 01:52:27 +0200

    Added customOpenApi to customize Swagger documentation.

--- src/main/java/cat/itacademy/barcelonactiva/mznmon/s05/t02/S05T02DiceGameApplication.java
+++ src/main/java/cat/itacademy/barcelonactiva/mznmon/s05/t02/S05T02DiceGameApplication.java
@@ -1,13 +1,30 @@
 package cat.itacademy.barcelonactiva.mznmon.s05.t02;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class S05T02DiceGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(S05T02DiceGameApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Dice Game API")
+						.version("0.1")
+						.description("API with Spring Boot to manage a Dice Game")
+						.termsOfService("http://swagger.io/terms")
+						.license(new License().name("Apache 2.0")
+								.url("http://springdoc.org")
+						));
 	}
 
 }
