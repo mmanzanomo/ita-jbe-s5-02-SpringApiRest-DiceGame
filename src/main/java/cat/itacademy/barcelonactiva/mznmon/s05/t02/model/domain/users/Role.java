cd2b6f2cad1609e51ff65ead98370eb553bf333e
Miguel Ángel Manzano <mznmiguel@gmail.com>
2023-06-16 17:39:05 +0200

    Added User and Role with MySql persistence entities.

--- src/main/java/cat/itacademy/barcelonactiva/mznmon/s05/t02/model/domain/users/Role.java
+++ src/main/java/cat/itacademy/barcelonactiva/mznmon/s05/t02/model/domain/users/Role.java
@@ -1,2 +1,19 @@
-package cat.itacademy.barcelonactiva.mznmon.s05.t02.model.domain.users;public class Role {
+package cat.itacademy.barcelonactiva.mznmon.s05.t02.model.domain.users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role")
+    private int idRole;
+    private String name;
 }
--- src/main/java/cat/itacademy/barcelonactiva/mznmon/s05/t02/model/domain/users/User.java
+++ src/main/java/cat/itacademy/barcelonactiva/mznmon/s05/t02/model/domain/users/User.java
@@ -1,2 +1,30 @@
-package cat.itacademy.barcelonactiva.mznmon.s05.t02.model.domain.users;public class User {
+package cat.itacademy.barcelonactiva.mznmon.s05.t02.model.domain.users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+    private String email;
+    @Column(unique = true, nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role")
+    private Role role;
+
+
+
 }
